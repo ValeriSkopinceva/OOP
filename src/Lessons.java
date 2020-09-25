bfd1613a6934b40f0a0723c2be7d6df8187a5efd
Valeria-bit <valerka_sk02@mail.ru>
2020-09-25 23:14:59 +0300

    Initial commit

---
+++ src/Lessons.java
@@ -0,0 +1,22 @@
+public class Lessons {
+    private String teacher;
+    private String lesson;
+
+    public void setTeacher(String teacher){
+        this.teacher = teacher;
+    }
+
+    private String getTeacher(){
+        return this.teacher;
+    }
+
+    public void setLesson(String lesson){
+        this.lesson = lesson;
+    }
+
+    private String getLesson(){
+        return this.lesson;
+    }
+
+
+}
